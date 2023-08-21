32baa73c7272508c094cfd5bcf131f6e5fe0e0b2
Leandro <lerocha644@gmai.com>
2023-08-21 19:09:51 -0300

    🚀 feat: Implement Payment

---
+++ src/main/java/com/leandro/dscommerce/Entity/Category.java
@@ -0,0 +1,26 @@
+package com.leandro.dscommerce.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    
+}
